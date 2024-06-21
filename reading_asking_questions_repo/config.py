40ef6e9da6fb2d0cf4330c4f580483eec6c07b64
ds2006 <87537875+ds2006@users.noreply.github.com>
2024-06-21 10:59:53 -0700

    added comments_removed model_name

---
+++ reading_asking_questions_repo/config.py
@@ -0,0 +1,6 @@
+#config.py     
+"""file that contains the color combinations for the various answers outputs. helps visually understand what is going on within the LLM"""
+WHITE = "\033[37m"
+GREEN = "\033[32m"
+RESET_COLOR = "\033[0m"
+
